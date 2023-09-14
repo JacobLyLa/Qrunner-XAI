1177c302cb2f5216c6ea1cd3e1b0e55d23022314
jacoblyla <jacob.llarsen@hotmail.com>
2023-09-14 14:22:59 +0200

    added xgb

--- cav.py
+++ cav.py
@@ -1,16 +1,19 @@
 import os
 
+import matplotlib.pyplot as plt
 import numpy as np
 import torch
-import torch.nn as nn
+import xgboost as xgb
 from PIL import Image
-from sklearn import linear_model, metrics
+from sklearn import linear_model
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
 from concepts import concept_instances
 from dqn import load_model
 from utils import prepare_folders
-import matplotlib.pyplot as plt
+
+# TODO: make CAV normalized and dont reshape it?
 
 class CAV:
     def __init__(self, concept, random_concept, q_network, activation_layers, device):
@@ -23,7 +26,8 @@ class CAV:
         self.activation_outputs = []
         self.cavs = {layer: None for layer in activation_layers}
         self.linear_models = {layer: None for layer in activation_layers}
-        self.test_set_accuracies = {layer: None for layer in activation_layers}
+        self.lm_test_set_accuracies = {layer: None for layer in activation_layers}
+        self.xgb_test_set_accuracies = {layer: None for layer in activation_layers}
 
         # prepare concept presence and absence observations
         self.concept_presence_obs = concept.get_presence_observations()
@@ -72,10 +76,23 @@ class CAV:
         # Fit linear model on the data
         lm = linear_model.SGDClassifier()
         lm.fit(x_train, y_train)
-        test_set_accuracy = lm.score(x_test, y_test)
-        print(f"Test set accuracy for concept {self.concept.name} in layer {layer}:", test_set_accuracy)
-        self.test_set_accuracies[layer] = test_set_accuracy
+        test_set_accuracy = accuracy_score(y_test, lm.predict(x_test))
+        print(f"[{layer}] Linear test accuracy:", test_set_accuracy)
+        self.lm_test_set_accuracies[layer] = test_set_accuracy
         self.linear_models[layer] = lm
+
+        # Fit XGBoost model on the data
+        dtrain = xgb.DMatrix(x_train, label=y_train)
+        dtest = xgb.DMatrix(x_test, label=y_test)
+        param = {'max_depth': 3, 'eta': 1, 'objective': 'binary:logistic'}
+        param['eval_metric'] = 'auc'
+        evallist = [(dtest, 'eval'), (dtrain, 'train')]
+        num_round = 5
+        bst = xgb.train(params=param, dtrain=dtrain, num_boost_round=num_round, evals=evallist, verbose_eval=False)
+        preds = bst.predict(dtest)
+        test_set_accuracy = accuracy_score(y_test, preds.round())
+        print(f"[{layer}] XGB test accuracy:", test_set_accuracy)
+        self.xgb_test_set_accuracies[layer] = test_set_accuracy
 
         # Save the CAV
         self.cavs[layer] = -lm.coef_[0]
@@ -84,16 +101,18 @@ class CAV:
         with torch.no_grad():
             self.q_network(self.concept_presence_obs)
             self.q_network(self.concept_absence_obs)
+        print(f"Generating CAVs for concept {self.concept.name}...")
         for layer in self.activation_layers:
             self._generate_cav(layer)
         # save cavs to disk
-        self.concept.save_cavs(self.cavs, self.activation_layers)
+        cavs = [self.cavs[layer] for layer in self.activation_layers]
+        self.concept.save_cavs(cavs, self.activation_layers)
         # clear activation outputs
         self.activation_outputs = []
 
     def sort_random_images(self):
         # choose CAV with highest test set accuracy
-        best_layer = max(self.test_set_accuracies, key=self.test_set_accuracies.get)
+        best_layer = max(self.lm_test_set_accuracies, key=self.lm_test_set_accuracies.get)
         best_layer_index = self.activation_layers.index(best_layer)
         print(f"best layer: {best_layer}")
         cav = self.cavs[best_layer]
@@ -116,7 +135,7 @@ class CAV:
     def plot_accuracies(self):
         # plot accuracies
         x = np.arange(len(self.activation_layers))
-        y = np.array(list(self.test_set_accuracies.values()))
+        y = np.array(list(self.lm_test_set_accuracies.values()))
         plt.plot(x, y, marker="o", linestyle="dashed", label=self.concept.name)
         plt.xticks(x, self.activation_layers)
         plt.ylim(0.0, 1.1)
@@ -125,6 +144,24 @@ class CAV:
         plt.ylabel("Accuracy")
         plt.legend()
         # plt.show()
+
+    def save_accuracies(self):
+        x = np.arange(len(self.activation_layers))
+        y1 = np.array(list(self.lm_test_set_accuracies.values()))
+        y2 = np.array(list(self.xgb_test_set_accuracies.values()))
+
+        plt.clf()
+
+        plt.plot(x, y1, marker="o", linestyle="dashed", label="Linear")
+        plt.plot(x, y2, marker="o", linestyle="dashed", label="XGB")
+        plt.xticks(x, self.activation_layers)
+        plt.ylim(0.0, 1.1)
+        plt.title(f"Accuracy vs Layer for concept: {self.concept.name}")
+        plt.xlabel("DQN layer")
+        plt.ylabel("Test accuracy")
+        plt.legend()
+        # save in cavs folder
+        plt.savefig(f"concepts/{self.concept.name}/cavs/accuracies.png")
 
     def remove_hooks(self):
         for hook in self.hooks:
@@ -136,11 +173,13 @@ q_network = load_model("runs/dqn_breakout/20230901-100931/models/model_5500000.p
 for i, layer in enumerate(q_network.network):
     print(f"Layer {i}: {layer}")
 random_concept = concept_instances[0]
-activation_layers = [1,3,5,8]
+# activation_layers = [1,3,5,8]
+activation_layers = list(range(1, 9))
 for concept in concept_instances:
     cav = CAV(concept, random_concept, q_network, activation_layers, device)
     cav.generate_cavs()
-    cav.sort_random_images()
-    cav.plot_accuracies()
+    #cav.sort_random_images()
+    cav.save_accuracies()
+    #cav.plot_accuracies()
     cav.remove_hooks()
 plt.show()
